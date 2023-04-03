bcb7e0e91a934754a65875bd7cfe98b1c6bd73f3
Ozanne Alexander <alexanoz@outlook.com>
2023-04-02 21:38:46 -0400

    test commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Resume from "./components/Portfolio";
+import Nav from "./components/Navi";
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
