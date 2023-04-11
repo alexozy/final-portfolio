f2c47fd1fc08da7fd47d84c8ff081ba65db15cb1
Ozanne Alexander <alexanoz@outlook.com>
2023-04-10 23:04:29 -0400

    fixed image directory

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 // import './App.css';
 import Footer from "./components/Footer";
 import Nav from "./components/Nav"
-// import Contact from "./components/Contact";
-// import Resume from "./components/Portfolio";
-
-// import photo from ‘../assets/sunselfie.jpeg’;
-// react image tag
-// <img width=“150” height=“200" src={photo}/>
 
 function App() {
   const [currentTab, setCurrentTab] = useState("Home")
